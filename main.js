5883d6062cad06dd1645e02340c553d5cae98515
ZainabZeeshan-dev <125381822+ZainabZeeshan-dev@users.noreply.github.com>
2024-03-08 23:46:10 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+//Array
+var students = ["Zainab", "Rafey", "Ahmed", "Hania", 1, 11, true];
+var firststudentname = students[0];
+console.log(students);
+console.log(firststudentname);
+console.log(students[1]);
+console.log(students[5]);
+console.log(students[0], students[1]);
+// Array Of string
+var fruitnames = ["apple", "mango", "grapes"];
+console.log(fruitnames);
+//Camel Case
+var myName = 'Zainab';
+//Snake Case
+var middle_name = 'zeeshan';
+//PascalCase
+var LastName = 'Shafiq';
+console.log(myName, middle_name, LastName);
---
+++ main.ts
@@ -0,0 +1,18 @@
+//Array
+let students = ["Zainab","Rafey","Ahmed","Hania",1,11,true]
+let firststudentname = students[0]
+console.log(students)
+console.log(firststudentname)
+console.log(students[1])
+console.log(students[5])
+console.log(students[0],students[1])
+// Array Of string
+let fruitnames: string[] = ["apple","mango","grapes"]
+console.log(fruitnames)
+//Camel Case
+let myName = 'Zainab';
+//Snake Case
+let middle_name = 'zeeshan';
+//PascalCase
+let LastName = 'Shafiq'
+console.log(myName,middle_name,LastName)
